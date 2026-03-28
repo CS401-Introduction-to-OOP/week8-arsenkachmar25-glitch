03711073fb75ced1e079ee07ef18bb9b097255b4
ArsenKachmar <a.kachmar23072008@gmail.com>
2026-03-28 14:44:48 +0200

    Final commit

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -4,26 +4,27 @@ class Program
 {
     static void Main()
     {
-        Letter myLetter = new Letter("LT-123456", 0.5);
-        Parcel myParcel = new Parcel("PC-987654", 2.5, "10x10x5 cm");
-        
-        List<DeliveryItem> deliveries = new List<DeliveryItem>
-        {
-            myLetter,
-            myParcel
-        };
-        
-        Console.WriteLine("Delivery Items");
-        
-        foreach (DeliveryItem item in deliveries)
-        {
-            Console.WriteLine(); 
-            
-            item.PrintInfo(); 
+        Letter letter1 = new Letter("LT-0001", 0.3);
+        Letter letter2 = new Letter("LT-0002", 0.7);
+        Parcel parcel1 = new Parcel("PC-0001", 2.5, "10x15x5 cm");
+        Parcel parcel2 = new Parcel("PC-0002", 5.0, "20x20x20 cm");
 
-            double cost = item.CalculateCost();
-            Console.WriteLine($"Shipping Cost : ${cost}");
-        }
+        Console.WriteLine("Polymorphism:");
+        
+        letter1.PrintInfo();
+        parcel1.PrintInfo();
+
+        CargoContainer<DeliveryItem> myCargo = new();
+
+        Console.WriteLine("Adding Items to ur cargo:");
+        
+        myCargo.AddItem(letter1);
+        myCargo.AddItem(letter2);
+        myCargo.AddItem(parcel1);
+        myCargo.AddItem(parcel2);
+        
+        double totalCost = myCargo.GetTotalCost();
+        Console.WriteLine($"Total Delivery Cost: {totalCost}");
         
     }
 }
